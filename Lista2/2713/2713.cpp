1800964a95706480b8eeeb0671455d8ddcf8dd75
arthursl12 <arthursoutolima@gmail.com>
2022-05-24 10:22:08 -0300

    Accepted 2713

--- Lista2/2713/2713.cpp
+++ Lista2/2713/2713.cpp
@@ -16,24 +16,23 @@ using namespace std;
 
 #define MAX_ROOMS 1234
 #define BIGM 10000000
+#define MAX_MANA 105
 
 int monster_hp[MAX_ROOMS];
 int dmg_increase[MAX_ROOMS];
 int n_rooms, in_mana, base_dmg;
 
 /*
-M[i][0]: menor tempo gasto total a partir da sala i sem usar mana nela
-M[i][1]: menor tempo gasto total a partir da sala i gastando mana nela
-
-min (M[n_rooms][0], M[n_rooms][1]) dá a resposta
+M[i][j]: menor tempo gasto total a partir da sala i, entrando com j mana
+M[0][in_mana] dá a resposta
 */
 
-float M[MAX_ROOMS][2];
+double M[MAX_ROOMS][MAX_MANA];
 
-float min_time(int i, int mana){
-    if (M[i][0] != BIGM && M[i][1] != BIGM){
+double min_time(int i, int mana){
+    if (M[i][mana] != BIGM){
         // Already seen case
-        return min(M[i][0], M[i][1]);
+        return M[i][mana];
     }
     
     if (i >= n_rooms){
@@ -42,27 +41,30 @@ float min_time(int i, int mana){
     }
     
     int enemy_hp = monster_hp[i];
+    double base_time = ((double) enemy_hp) / base_dmg;
     if(mana >= 1){
-        float mana_time = ((float) enemy_hp) / (base_dmg+dmg_increase[i]);
-        M[i][1] = min(M[i][1], mana_time+min_time(i+1, mana-1));
-    }else{
-        float base_time = ((float) enemy_hp) / base_dmg;
-        M[i][0] = min(M[i][0], base_time+min_time(i+1, mana));
+        double mana_time = ((double) enemy_hp) / (base_dmg+dmg_increase[i]);
+        M[i][mana] = min(base_time+M[i][mana], mana_time+min_time(i+1, mana-1));
     }
-    return min(M[i][0], M[i][1]);
+
+    M[i][mana] = min(base_time+M[i][mana], base_time+min_time(i+1, mana));
+    
+    return M[i][mana];
 
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cout << fixed;
-    cout << setprecision(3);
+    cout << setprecision(4);
     
     while(cin >> n_rooms >> in_mana >> base_dmg){
         // memset(M, BIGM, 7*sizeof(int));
         for(int i=0; i<n_rooms; i++){
-            M[i][0] = BIGM;
-            M[i][1] = BIGM;
+            for (int j=0; j<=in_mana; j++){
+                M[i][j] = BIGM;
+                M[i][j] = BIGM;
+            }
         }
 
         memset(monster_hp, -1, MAX_ROOMS*sizeof(int));
@@ -73,7 +75,38 @@ int main() {
             dmg_increase[i] = increase;
         }
 
-        cout << min_time(0, in_mana) << endl;
+        // Filling last room times
+        int idx = n_rooms-1;
+        int enemy_hp = monster_hp[idx];
+        double base_time = ((double) enemy_hp) / base_dmg;
+        M[idx][0] = base_time;
+        double mana_time = ((double) enemy_hp) / (base_dmg+dmg_increase[idx]);
+        for(int mana=1; mana<=in_mana; mana++){
+            M[idx][mana] = mana_time;
+        }
+
+        for(int i=n_rooms-2; i>=0; i--){
+            enemy_hp = monster_hp[i];
+            base_time = ((double) enemy_hp) / base_dmg;
+
+            // Without mana, there is no choice
+            M[i][0] = base_time+M[i+1][0];
+
+            // With mana available, one must choose using it or not
+            mana_time = ((double) enemy_hp) / (base_dmg+dmg_increase[i]);
+            for(int mana=1; mana<=in_mana; mana++){
+                M[i][mana] = min(base_time+M[i+1][mana],mana_time+M[i+1][mana-1]);
+            }
+            
+        }
+
+        cout << M[0][in_mana] << endl;
+
+
+
+
+
+        // cout << min_time(0, in_mana) << endl;
 
     }
     return 0;
