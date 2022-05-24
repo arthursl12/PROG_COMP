71c2be2f6db00dd4cb16e35f88012ca526e248f5
arthursl12 <arthursoutolima@gmail.com>
2022-05-24 08:36:17 -0300

    Accepted 1362

--- Lista2/1362/1362.cpp
+++ Lista2/1362/1362.cpp
@@ -1,61 +1,3 @@
-// #define MAXN 31
-// int capacidade,n,TC;
-// map<string,int> conversao;
-// vector<int> grafo[MAXN],vis;
-// vector< vector<int> > match;
-// int augmenting_path(int l){
-// 	if(vis[l]) return 0;
-// 	vis[l] = 1;
-// 	for(int i=0;i<grafo[l].size();i++){
-// 		int r = grafo[l][i];
-// 		if(match[r].size() < capacidade){
-// 			match[r].push_back(l);
-// 			return 1;
-// 		}
-// 		for(int j = 0; j < match[r].size();j++){
-// 			int k = match[r][j];
-// 			if(augmenting_path(k)){
-// 				match[r][j] = l;
-// 				return 1;
-// 			}
-// 		}
-// 	}
-// 	return 0;
-// }
-// int main(){
-// 	ios_base::sync_with_stdio(0);
-// 	cin.tie(0);
-// 	cout.tie(0);
-// 	cin >> TC;
-// 	conversao["XXL"] = 31;
-// 	conversao["XL"] = 32;
-// 	conversao["L"] = 33;
-// 	conversao["M"] = 34;
-// 	conversao["S"] = 35;
-// 	conversao["XS"] = 36;
-// 	while(TC--){
-// 		cin >> capacidade >> n;
-// 		capacidade = capacidade / 6;
-// 		for(int i=1;i<=n;i++){
-// 			string a,b;
-// 			cin >> a >> b;
-// 			grafo[i].clear();
-// 			grafo[i].push_back(conversao[a]);
-// 			grafo[i].push_back(conversao[b]);
-// 		}
-// 		vector<int> temp;
-// 		match.assign(40,temp);
-// 		int resp = 0;
-// 		for(int i=1;i<=n;i++){
-// 			vis.assign(40,0);
-// 			resp += augmenting_path(i);
-// 		}
-// 		if(resp == n) cout << "YES" << endl;
-// 		else cout << "NO" << endl;
-// 	}
-// 	return 0;
-// }
-
 #include <iostream>
 #include <stdlib.h>
 #include <cstring>
@@ -69,14 +11,14 @@
 using namespace std;
 
 #define INF 0x3f3f3f
-#define MAX 12345
-#define MAX_DIFF 5
+#define MAX 40
 #define ii pair<int,int>
 
-int M[MAX][6][6];
-int weight[MAX];
+int available_shirts[7];
 int N;
-
+int n_shirts_each, n_compet;
+vector<vector<int>> graph;
+bool possible;
 
 // (XXL, XL, L, M , S, or XS).
 int size2int(string size){
@@ -97,17 +39,62 @@ int size2int(string size){
     }
 }
 
+void print_vec(){
+    for(int i=1; i<=7; i++){
+        cout << available_shirts[i] << ",";
+    }
+    cout << endl;
+}
+
+
+void assign(int i){
+    // print_vec();
+    if (i == n_compet){
+        // It was possible to fit everything
+        possible = true;
+    }
+    if (possible){
+        return;
+    }
+
+    // Try using one option of shirt
+    if (available_shirts[graph[i][0]] > 0){
+        available_shirts[graph[i][0]] -= 1;
+        assign(i+1);
+        available_shirts[graph[i][0]] += 1;
+    }
+
+    // Try using other option of shirt
+    if (available_shirts[graph[i][1]] > 0){
+        available_shirts[graph[i][1]] -= 1;
+        assign(i+1);
+        available_shirts[graph[i][1]] += 1;
+    }
+
+
+}
+
+
+
+
 int main() {
     ios_base::sync_with_stdio(false);
 
     int n_tests; cin >> n_tests;
     for(int iiii=0; iiii<n_tests; iiii++){
+        memset(available_shirts, 0, 7*sizeof(int));
+
         // Input
         int n_shirts; cin >> n_shirts;
-        int n_compet; cin >> n_compet;
+        n_shirts_each = n_shirts / 6;
+        for(int i=1; i<=6; i++){
+            available_shirts[i] = n_shirts_each;
+        }
+        // print_vec();
+        cin >> n_compet;
 
         // Bipartite graph from competitors -> accepted sizes
-        vector<vector<int>> graph;
+        graph.clear();
         for(int i=0; i<n_compet; i++){
             string size1; cin >> size1; 
             string size2; cin >> size2;
@@ -118,8 +105,14 @@ int main() {
 
             graph.push_back(accepted);
         }
+        possible = false;
+        assign(0);
 
-
+        if (possible){
+            cout << "YES" << endl;
+        }else{
+            cout << "NO" << endl;
+        }
     }
     return 0;
 }
