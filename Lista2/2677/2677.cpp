73a39d338f59a12cfdc89e4cc8aba129838e8d37
arthursl12 <arthursoutolima@gmail.com>
2022-06-05 08:41:58 -0300

    Accepted 2677

--- Lista2/2677/2677.cpp
+++ Lista2/2677/2677.cpp
@@ -10,16 +10,13 @@
 
 using namespace std;
 
-#define INF 0x3f3f3f
+#define INF 0x3f3f3f3f
 #define ii pair<int,int>
-
-
 #define MAX 1234
 
 int N;
-int max_even[MAX][MAX][2];
+int max_even[MAX][MAX];
 int numbers[MAX];
-
 
 int isEven(int i){
     return (numbers[i] % 2 == 0);
@@ -35,56 +32,15 @@ void print_vec(){
 void print_mat(){
     for(int i=1; i<=2*N; i++){
         for(int j=1; j<=2*N; j++){
-            if (max_even[i][j][true] == -1){
+            if (max_even[i][j] == -1){
                 cout << "*" << ",";
             }else{
-                cout << max_even[i][j][true] << ",";
-            }
-
-            if (max_even[i][j][false] == -1){
-                cout << "*" << "  ";
-            }else{
-                cout << max_even[i][j][false] << "  ";
+                cout << max_even[i][j] << ",";
             }
         }
         cout << endl;
     }
     cout << endl;
-}
-
-int max_score;
-void run_game(int i, int j, bool vez, int curr_score){
-    // cout << "Round(" << vez << "): " << i << "," << j << ", sc=" << curr_score << endl; 
-    if(vez){
-        if(i == j){
-            curr_score += isEven(i);
-            max_score = max(max_score,curr_score);
-        }else if(isEven(i) && !isEven(j)){
-            // Choose i
-            run_game(i+1, j, !vez, curr_score+1);
-        }else if(isEven(i) && !isEven(j)){
-            // Choose j
-            run_game(i, j-1, !vez, curr_score+1);
-        }else{
-            // Try each
-            run_game(i+1, j, !vez, curr_score+isEven(i));
-            run_game(i, j-1, !vez, curr_score+isEven(j));
-        }
-    }else{
-        if(i == j){
-            max_score = max(max_score,curr_score);
-        }else if(isEven(i) && !isEven(j)){
-            // P2 Choose i
-            run_game(i+1, j, !vez, curr_score);
-        }else if(isEven(i) && !isEven(j)){
-            // P2 Choose j
-            run_game(i, j-1, !vez, curr_score);
-        }else{
-            // Try each
-            run_game(i+1, j, !vez, curr_score);
-            run_game(i, j-1, !vez, curr_score);
-        }
-    }
 }
 
 
@@ -93,17 +49,29 @@ int main() {
     
     cin >> N;
     while(N != 0){
-        max_score = 0;
         memset(numbers, -1, MAX*sizeof(int));
 
-        for(int i=1; i<=2*N; i++){
+        for(int i=0; i<2*N; i++){
             int numb; cin >> numb;
             numbers[i] = numb;
         }
+
         // print_vec();
         // print_mat();
-        run_game(1,2*N,true,0);
-        cout << max_score << endl;
+        for (int i=0; i<2*N; i++){
+            for (int j=0; i+j<2*N; j++){
+                int k = i+j;
+                if (i == 0)
+                    max_even[j][k] = 0;
+
+                if (i % 2 == 0)
+                    max_even[j][k] = min(max_even[j + 1][k], max_even[j][k -1]);
+                else
+                    max_even[j][k] = max(max_even[j + 1][k] + isEven(j), max_even[j][k - 1] + isEven(k));
+            }
+        }
+
+        cout << max_even[0][2*N - 1] << endl;
 
         // cout << calculate(1,2*N, true) << endl;
         // print_mat();
